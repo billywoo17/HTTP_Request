4b362d7706faea4879a22277244e9d5554498653
bill <billwu_@outlook.com>
2018-04-16 19:31:35 +0000

    finished step 2

---
+++ PrintingBufferedStrings.js
@@ -0,0 +1,22 @@
+function getAndPrintHTMLChunks () {
+  var https = require('https');
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  https.get(requestOptions, function (response) {
+    response.setEncoding('utf8');
+
+    var chunk = "";
+
+    response.on('data', function (data) {
+      chunk += data;
+    });
+
+    response.on('end', function() {
+      console.log(chunk);
+    });
+  });
+}
+getAndPrintHTMLChunks();
