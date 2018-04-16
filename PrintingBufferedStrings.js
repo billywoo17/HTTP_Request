b1ad952398163a3d0bbbfe57a9be23f192a4d477
billywoo17 <37318190+billywoo17@users.noreply.github.com>
2018-04-16 12:23:41 -0700

    Update PrintingBufferedStrings.js

--- PrintingBufferedStrings.js
+++ PrintingBufferedStrings.js
@@ -2,7 +2,7 @@ function getAndPrintHTMLChunks () {
   var https = require('https');
   var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    path: '/http-examples/step2.html'
   };
 
   https.get(requestOptions, function (response) {
