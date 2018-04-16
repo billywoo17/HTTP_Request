84d6ea45f4e723047178601bbbf5aa5b5c67e676
Bill <billwu_@outlook.com>
2018-04-16 11:31:20 -0700

    finished step 1

---
+++ HTTPRequest.js
@@ -0,0 +1,22 @@
+function getAndPrintHTMLChunks () {
+  var https = require('https');
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function (response) {
+    response.setEncoding('utf8');
+
+    var chunk = "";
+
+    response.on('data', function (data) {
+      chunk += data + '\n';
+    });
+
+    response.on('end', function() {
+      console.log(chunk);
+    });
+  });
+}
+getAndPrintHTMLChunks();
